1cf7fbdf11edd92fd951c82b4edf0969153fdee7
Recursif <jillian.dum@gmail.com>
2018-08-24 13:28:10 +0200

    add data component

---
+++ simple-vue/data-component/src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>Hello {{person.firstname}} {{person.lastname}} from {{from}}!!!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      person: {
+        firstname: 'Homer',
+        lastname: 'Simpson',
+      },
+      from: 'Spingfield',
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+</style>
