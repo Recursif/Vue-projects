96eb32e69adae5b1bb6ed25976b145d3c1897b70
Recursif <jillian.dum@gmail.com>
2018-08-30 11:16:31 +0200

    add store backed counter

--- vuex/vuex-test/src/components/Counter.vue
+++ vuex/vuex-test/src/components/Counter.vue
@@ -2,20 +2,25 @@
   <div>
     {{ count }}
   </div>
+     <button @click="increment">+</button>
+    <button @click="decrement">-</button>
 </template>
 
 <script>
 export default {
   name: 'Counter',
-  data () {
-    return {
-      count: 0
+  computed: {
+    count () {
+	    return store.state.count
     }
   },
   // actions
   methods: {
     increment () {
-      this.count++
+      store.commit('increment')
+    },
+    decrement () {
+    	store.commit('decrement')
     }
   }
 }
