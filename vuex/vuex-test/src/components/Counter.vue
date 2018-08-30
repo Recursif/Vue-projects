c8410b9588acf0acd7e3633b2c1df4ac9f031625
Recursif <jillian.dum@gmail.com>
2018-08-30 10:59:47 +0200

    test the vuex example

---
+++ vuex/vuex-test/src/components/Counter.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    {{ count }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Counter',
+  data () {
+    return {
+      count: 0
+    }
+  },
+  // actions
+  methods: {
+    increment () {
+      this.count++
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
