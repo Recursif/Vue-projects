34e2815ab924fcb0d2ea5455124af08cd6293cae
Recursif <jillian.dum@gmail.com>
2018-08-31 09:36:32 +0200

    component login done

---
+++ vuex/auth-vuex/src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="hello">
+    Home
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+}
+</script>
+
+
+<style>
+
+</style>
---
+++ vuex/auth-vuex/src/components/Login.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="hola">
+    Login
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+}
+</script>
+
+
+<style>
+
+</style>
